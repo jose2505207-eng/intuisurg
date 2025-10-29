517d6cf3a19029a79b8a25dda6a5e7597089fd62
jose2505207-eng <jose2505207@hybridge.education>
2025-10-29 01:15:27 -0700

    Updated FeedbackModal.tsx

--- src/components/FeedbackModal.tsx
+++ src/components/FeedbackModal.tsx
@@ -82,7 +82,7 @@ export function FeedbackModal({
       case 1:
         return (
           <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-2a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clipRule="evenodd" />
+            <path d="M18 9.5a1.5 1.5 0 11-3 0v-6a1.5 1.5 0 013 0v6zM14 9.667v-5.43a2 2 0 00-1.105-1.79l-.05-.025A4 4 0 0011.055 2H5.64a2 2 0 00-1.962 1.608l-1.2 6A2 2 0 004.44 12H8v4a2 2 0 002 2 1 1 0 001-1v-.667a4 4 0 01.8-2.4l1.4-1.866a4 4 0 00.8-2.4z" transform="translate(0, 20) scale(1, -1)" />
           </svg>
         );
       case 2:
@@ -94,7 +94,7 @@ export function FeedbackModal({
       case 3:
         return (
           <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-2a1 1 0 11-2 0 1 1 0 012 0zm-7.536 5.879a1 1 0 001.415 0 3 3 0 014.242 0 1 1 0 001.415-1.415 5 5 0 00-7.072 0 1 1 0 000 1.415z" clipRule="evenodd" />
+            <path d="M2 10.5a1.5 1.5 0 003 0v-6a1.5 1.5 0 00-3 0v6zM6 10.333v-5.43a2 2 0 011.105-1.79l.05-.025A4 4 0 018.945 3h5.416a2 2 0 011.962 1.608l1.2 6A2 2 0 0115.56 13H12v4a2 2 0 01-2 2 1 1 0 01-1-1v-.667a4 4 0 00-.8-2.4L6.8 13.067a4 4 0 01-.8-2.4z" />
           </svg>
         );
     }
