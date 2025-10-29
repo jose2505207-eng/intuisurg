d1bd9429b4b1f3238005329945954aac08c7b2b6
jose2505207-eng <jose2505207@hybridge.education>
2025-10-29 01:12:09 -0700

    Updated FeedbackModal.tsx

--- src/components/FeedbackModal.tsx
+++ src/components/FeedbackModal.tsx
@@ -82,7 +82,7 @@ export function FeedbackModal({
       case 1:
         return (
           <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7zm0 4a1 1 0 000 2h.01a1 1 0 100-2H7zm5.99 0a1 1 0 000 2H13a1 1 0 100-2h-.01z" />
+            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-2a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clipRule="evenodd" />
           </svg>
         );
       case 2:
